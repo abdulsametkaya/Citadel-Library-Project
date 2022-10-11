cb723954641f45e400ab363eadd37bc84c96188d
Abdulsamet Kaya <aasametkaya@gmail.com>
2022-10-11 15:43:57 +0300

    initial commit

---
+++ src/main/java/com/library/citadel_library/CitadelLibraryProfectApplication.java
@@ -0,0 +1,13 @@
+package com.library.citadel_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitadelLibraryProfectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitadelLibraryProfectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/library/citadel_library/CitadelLibraryProfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.citadel_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitadelLibraryProfectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
