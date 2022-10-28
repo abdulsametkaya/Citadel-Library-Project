30f5ffe50e6355f0e5795b3b97497b4b316d950c
Abdulsamet Kaya <aasametkaya@gmail.com>
2022-10-28 19:45:46 +0300

    configurations

--- src/main/java/com/library/citadel_library/CitadelLibraryProfectApplication.java
+++ src/main/java/com/library/citadel_library/CitadelLibraryProfectApplication.java
@@ -2,6 +2,9 @@ package com.library.citadel_library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CitadelLibraryProfectApplication {
@@ -10,4 +13,14 @@ public class CitadelLibraryProfectApplication {
         SpringApplication.run(CitadelLibraryProfectApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
 }
